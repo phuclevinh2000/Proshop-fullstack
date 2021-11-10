c195ccdca14a3953f51d6f654863203acdc4c131
Phuc Le <phuclevinh2000@gmail.com>
2021-11-10 16:13:29 +0200

    add error handler

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+const notFound = (req, res, next) => { //throw error if wrong address
+  const error = new Error(`Not Found - ${req.originalUrl}`)
+  res.status(404)
+  next(error)
+}
+
+const errorHandler = (err, req, res, next) => {  //custom error handler
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  })
+}
+
+export { notFound, errorHandler }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
+
 import productRoutes from "./routes/productRoutes.js"
 
 dotenv.config()
@@ -15,6 +17,10 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
