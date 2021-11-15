f913ba8ad9aedf4bd7c48d91be110b301f25d889
Phuc Le <phuclevinh2000@gmail.com>
2021-11-15 15:30:37 +0200

    add to cart and localstore

---
+++ frontend/src/redux/actions/cartActions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { CART_ADD_ITEM,CART_REMOVE_ITEM } from "../../constants/cartConstants";
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`)
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  })
+  // console.log(data)
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))  //save to local storage
+}
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))  //save to local storage
+}
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,14 +1,25 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer, productDetailsReducer } from "./reducer/productReducer.js"
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducer/productReducer.js';
+import { cartReducer } from './reducer/cartReduders.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
