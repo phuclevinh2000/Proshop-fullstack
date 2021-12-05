a6ca4eeadf0f8f70bef712ac4b052f51b051f89e
Phuc Le <phuclevinh2000@gmail.com>
2021-12-05 17:41:10 +0200

    UPdate profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,4 +80,33 @@ const getUserProfile = asyncHandler(async (req, res, next) => {
     throw new Error('Invalid email or password');
   }
 });
-export { authUser, getUserProfile, registerUser };
+
+// @desc  Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+export { authUser, getUserProfile, registerUser,updateUserProfile };
